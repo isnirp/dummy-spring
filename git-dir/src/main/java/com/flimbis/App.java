88fea4ad4c45ec53006c1c9e555316a6c2b182b6
isnirp <pramankwah@gmail.com>
2023-09-28 23:00:48 +0200

    git-dir: async service

--- git-dir/src/main/java/com/flimbis/App.java
+++ git-dir/src/main/java/com/flimbis/App.java
@@ -1,13 +1,23 @@
 package com.flimbis;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
 /**
- * Hello world!
- *
+ * Git Directory
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public RestClient getRestClient() {
+        return RestClient.builder()
+                .baseUrl("https://api.github.com")
+                .build();
     }
 }
--- git-dir/src/main/java/com/flimbis/service/LookupService.java
+++ git-dir/src/main/java/com/flimbis/service/LookupService.java
@@ -1,2 +1,30 @@
-package com.flimbis.service;public class LookupService {
+package com.flimbis.service;
+
+import com.flimbis.model.User;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Service
+public class LookupService {
+    private final RestClient restClient;
+
+    public LookupService(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    @Async
+    public CompletableFuture<User> findByLogin(String user) {
+        User gitUser = restClient.get()
+                .uri("/users/{user}", user)
+                .accept(APPLICATION_JSON)
+                .retrieve()
+                .body(User.class);
+
+        return CompletableFuture.completedFuture(gitUser);
+    }
 }
